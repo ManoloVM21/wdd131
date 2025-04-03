da67c0c1b1be416c02a3fc020623dba302d1ec85
ManoloVM <mvasquezmendoza@byupathway.edu>
2025-04-03 12:35:56 -0500

    updates

---
+++ week05/scripts/bom-script.js
@@ -0,0 +1,30 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
+
+
+button.addEventListener('click',function(){
+    if (input.value.trim() != ""){
+        
+        const listElement = document.createElement("li");
+        const deleteButton = document.createElement("button");
+        listElement.textContent = input.value;
+        deleteButton.textContent = '❌';
+
+        listElement.append(deleteButton);
+        list.append(listElement);
+
+        deleteButton.addEventListener('click',function(){
+            list.removeChild(listElement);
+            input.focus();
+        })
+        input.value = "";
+        input.focus();
+    } else {
+        input.focus();
+    }
+    button.addEventListener('click', () =>{
+        menuitems.classList.toggle
+    })
+});
