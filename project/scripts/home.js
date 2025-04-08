e5d67fad68928bc16a0780ae49a2b2d7bd3def2c
ManoloVM <mvasquezmendoza@byupathway.edu>
2025-04-08 14:43:20 -0500

    home page added

---
+++ project/scripts/home.js
@@ -0,0 +1,29 @@
+const year = document.querySelector("#currentyear")
+const modified = document.querySelector("#lastModified");
+const today = new Date();
+
+year.innerHTML = today.getFullYear();
+modified.innerHTML = `Last Modification: <span id="currentyear">${new Intl.DateTimeFormat(
+    'en-US', {
+        dateStyle: 'medium',
+        timeStyle: "medium"
+    }).format(today)}</span>`;
+
+const valChamp = new Date("2025-09-04T00:00:00");
+const lolWorlds = new Date("2025-08-10T00:00:00");
+
+function getCountdown(date,id){
+    const now = new Date();
+    const timeRemaining = date - now;
+
+    if (timeRemaining <= 0){
+        document.getElementById(id).innerHTML = "Event has passed!";
+        return;
+    }
+    const days = Math.floor(timeRemaining / (1000*60*60*24))
+    const hours = Math.floor((timeRemaining % (1000*60*60*24))/(1000*60*60));
+    document.getElementById(id).textContent = `${days} days, ${hours} hours left`;
+}
+
+getCountdown(valChamp,"countdown-val");
+getCountdown(lolWorlds,"countdown-lol");
